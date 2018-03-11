264e0f9dfe5b9f2c43c6202c677cfaf447f6cf9b
yangfan <hyyangfan@gmail.com>
2018-03-11 19:12:02 +0800

    product

---
+++ src/main/java/com/roncoo/esjop/product/ha/EshopProductHaApplication.java
@@ -0,0 +1,12 @@
+package com.roncoo.esjop.product.ha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EshopProductHaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EshopProductHaApplication.class, args);
+	}
+}
---
+++ src/main/java/com/roncoo/esjop/product/ha/controller/ProductController.java
@@ -0,0 +1,20 @@
+package com.roncoo.esjop.product.ha.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 商品服务接口
+ *
+ * @author yangfan
+ * @date 2018/03/11
+ */
+@RestController
+public class ProductController {
+
+    @GetMapping("/getProductInfo")
+    public String getProductInfo(Long productId) {
+
+        return "{\"id\": " + 1 + ", \"name\": \"iphone7手机\", \"price\": 5599, \"pictureList\":\"a.jpg,b.jpg\", \"specification\": \"iphone7的规格\", \"service\": \"iphone7的售后服务\", \"color\": \"红色,白色,黑色\", \"size\": \"5.5\", \"shopId\": 2, \"modifiedTime\": \"2018-02-21 21:11:34\"}";
+    }
+}
