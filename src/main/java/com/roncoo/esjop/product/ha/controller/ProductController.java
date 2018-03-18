9a3a602deb7c572f2b5c8f76a80abf0debabeb79
yangfan <hyyangfan@gmail.com>
2018-03-18 21:03:54 +0800

    add cityId

--- src/main/java/com/roncoo/esjop/product/ha/controller/ProductController.java
+++ src/main/java/com/roncoo/esjop/product/ha/controller/ProductController.java
@@ -15,6 +15,6 @@ public class ProductController {
     @GetMapping("/getProductInfo")
     public String getProductInfo(Long productId) {
 
-        return "{\"id\": " + 1 + ", \"name\": \"iphone7手机\", \"price\": 5599, \"pictureList\":\"a.jpg,b.jpg\", \"specification\": \"iphone7的规格\", \"service\": \"iphone7的售后服务\", \"color\": \"红色,白色,黑色\", \"size\": \"5.5\", \"shopId\": 2, \"modifiedTime\": \"2018-02-21 21:11:34\"}";
+        return "{\"id\": " + productId + ", \"name\": \"iphone7手机\", \"price\": 5599, \"pictureList\":\"a.jpg,b.jpg\", \"specification\": \"iphone7的规格\", \"service\": \"iphone7的售后服务\", \"color\": \"红色,白色,黑色\", \"size\": \"5.5\", \"shopId\": 2, \"modifiedTime\": \"2018-02-21 21:11:34\", \"cityId\": 1}";
     }
 }
