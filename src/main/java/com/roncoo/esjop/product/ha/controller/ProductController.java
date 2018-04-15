1906e0dfb5bb67fb979544127ab5b2f7221b6747
yangfan <hyyangfan@gmail.com>
2018-04-15 11:44:19 +0800

    getProductInfos

--- src/main/java/com/roncoo/esjop/product/ha/controller/ProductController.java
+++ src/main/java/com/roncoo/esjop/product/ha/controller/ProductController.java
@@ -1,5 +1,7 @@
 package com.roncoo.esjop.product.ha.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,5 +18,20 @@ public class ProductController {
     public String getProductInfo(Long productId) {
 
         return "{\"id\": " + productId + ", \"name\": \"iphone7手机\", \"price\": 5599, \"pictureList\":\"a.jpg,b.jpg\", \"specification\": \"iphone7的规格\", \"service\": \"iphone7的售后服务\", \"color\": \"红色,白色,黑色\", \"size\": \"5.5\", \"shopId\": 2, \"modifiedTime\": \"2018-02-21 21:11:34\", \"cityId\": 1}";
+    }
+
+
+    @GetMapping("/getProductInfos")
+    public String getProductInfos(String productIds) {
+
+        JSONArray jsonArray = new JSONArray();
+
+        for (String productId : productIds.split(",")) {
+            String json = "{\"id\": " + productId + ", \"name\": \"iphone7手机\", \"price\": 5599, \"pictureList\":\"a.jpg,b.jpg\", \"specification\": \"iphone7的规格\", \"service\": \"iphone7的售后服务\", \"color\": \"红色,白色,黑色\", \"size\": \"5.5\", \"shopId\": 2, \"modifiedTime\": \"2018-02-21 21:11:34\", \"cityId\": 1}";
+            jsonArray.add(JSON.parse(json));
+        }
+
+        return jsonArray.toJSONString();
+
     }
 }
